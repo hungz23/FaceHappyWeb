e391b6f35abce88e0d2498d6db801a80e96883af
hungz23 <hungtranbuddha@gmail.com>
2018-01-12 02:09:29 +0000

    Add all

---
+++ mysite/facenet/src/emotiondetection.py
@@ -0,0 +1,27 @@
+#Emotion Detection Using Mememoji
+import cv2
+import sys
+import json
+import time
+import numpy as np
+from keras.models import model_from_json
+
+emotion_labels = ['angry', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+
+# load json and create model arch
+json_file = open('model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+
+# load weights into new model
+model.load_weights('model.h5')
+
+def predict_emotion(face_image_gray): # a single cropped face
+    resized_img = cv2.resize(face_image_gray, (48,48), interpolation = cv2.INTER_AREA)
+    # cv2.imwrite(str(index)+'.png', resized_img)
+    image = resized_img.reshape(1, 1, 48, 48)
+    list_of_list = model.predict(image, batch_size=1, verbose=1)
+    angry, fear, happy, sad, surprise, neutral = [prob for lst in list_of_list for prob in lst]
+    return [angry, fear, happy, sad, surprise, neutral]
